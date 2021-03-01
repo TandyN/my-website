d7b5c82275f300dee9fe7b22f25e51d6df85b89b
Tandy Nguyen <nguyetan001@gmail.com>
2021-03-01 15:00:11 -0500

    Center category items to page

--- src/components/SectionSkills/index.jsx
+++ src/components/SectionSkills/index.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
+const gridCategory = makeStyles({
+  root: {
+    width: '50%',
+  },
+});
 
 const SectionSkills = () => {
+  const gridCategoryClass = gridCategory();
+
   return (
-    <Grid container spacing={10} direction="column">
-      <Grid item>
+    <Grid 
+      container
+      spacing={0}
+      alignItems="center"
+      justify="center"
+      direction="column"
+      >
+      <Grid item className={gridCategoryClass.root} >
         <Button>Frontend</Button>
       </Grid>
-      <Grid item>
+      <Grid item className={gridCategoryClass.root}>
         <Button>Backend</Button>
       </Grid>
-      <Grid item>
+      <Grid item className={gridCategoryClass.root}>
         <Button>Databases</Button>
       </Grid>
-      <Grid item>
+      <Grid item className={gridCategoryClass.root}>
         <Button>Testing</Button>
       </Grid>
     </Grid>
