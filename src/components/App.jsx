615ba8fbb700ecff245fad235c22cc7f44d2eabd
Tandy Nguyen <nguyetan001@gmail.com>
2021-03-02 18:40:00 -0500

    Add scroll handlers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import ReactScrollWheelHandler from 'react-scroll-wheel-handler';
 import MenuBar from './MenuBar';
 import SectionHome from './SectionHome';
 import SectionSkills from './SectionSkills';
@@ -7,14 +8,22 @@ import SectionExperience from './SectionExperience';
 import SectionPortfilio from './SectionPortfolio';
 
 const App = () => {
+  const [currentPage, setCurrentPage] = React.useState((0));
+
+
   return (
-    <Container maxWidth="xl">
-      <MenuBar />
-      <SectionHome />
-      <SectionSkills />
-      <SectionExperience />
-      <SectionPortfilio />
-    </Container>
+    <ReactScrollWheelHandler
+      upHandler={() => { if (currentPage > 0) setCurrentPage(currentPage - 1) }}
+      downHandler={() => { if (currentPage < 4) setCurrentPage(currentPage + 1) }}
+    >
+      <Container maxWidth="xl">
+        <MenuBar />
+        <SectionHome />
+        <SectionSkills />
+        <SectionExperience />
+        <SectionPortfilio />
+      </Container>
+    </ReactScrollWheelHandler>
   )
 }
 
