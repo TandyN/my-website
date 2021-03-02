c3d5d105618ecc1afd19ec9818d291584b0f8c62
Tandy Nguyen <nguyetan001@gmail.com>
2021-03-02 09:04:24 -0500

    Experience data is displayed

--- src/components/SectionExperience/index.jsx
+++ src/components/SectionExperience/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import ExperienceItem from './ExperienceItem';
 
 import experienceData from './experience.json'; 
 
@@ -11,6 +12,11 @@ const SectionExperience = () => {
       direction='column'
     >
       <h2>Experience</h2>
+      {experienceData.map((experienceItem) => {
+        return (
+          <ExperienceItem experienceItem={experienceItem} />
+        )
+      })}
     </Grid>
   )
 }
