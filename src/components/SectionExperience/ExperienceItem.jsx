d80a04471a227edda46316badb503124988ca867
Tandy Nguyen <nguyetan001@gmail.com>
2021-03-02 13:00:05 -0500

    Align experience items to the left

--- src/components/SectionExperience/ExperienceItem.jsx
+++ src/components/SectionExperience/ExperienceItem.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
-
 const ExperienceItem = ({ experienceItem }) => {
   return (
     <Grid
--- src/components/SectionExperience/index.jsx
+++ src/components/SectionExperience/index.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
 import ExperienceHeader from './ExperienceHeader';
 import ExperienceItem from './ExperienceItem';
 
 import experienceData from './experience.json';
 
+const experienceItemsContainer = makeStyles({
+  root: {
+    margin: 'auto',
+    width: '50%',
+  },
+});
+
 const SectionExperience = () => {
+  const experienceItemsContainerClass = experienceItemsContainer();
+
   return (
     <Grid
       container
-      direction='column'
+      direction="column"
+      alignItems="center"
+      className={experienceItemsContainerClass.root}
     >
       <ExperienceHeader />
-      <Box>
+      <Grid
+        container
+        direction="column"
+        justify="space-evenly"
+      >
         {experienceData.map((experienceItem, index) => {
           return (
             <ExperienceItem experienceItem={experienceItem} key={index} />
           )
         })}
-      </Box>
+      </Grid>
     </Grid>
   )
 }
