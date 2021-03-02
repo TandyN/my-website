5ef53a2ab77579c6427109d53a2b418a8a8b55bc
Tandy Nguyen <nguyetan001@gmail.com>
2021-03-02 11:16:57 -0500

    Add hide/show functionality to skills

--- src/components/SectionSkills/index.jsx
+++ src/components/SectionSkills/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import FrontendItems from './FrontendItems';
@@ -15,6 +16,26 @@ const gridCategory = makeStyles({
 
 const SectionSkills = () => {
   const gridCategoryClass = gridCategory();
+  const [frontendDisplay, setFrontendDisplay] = React.useState('none');
+  const [backendDisplay, setBackendDisplay] = React.useState('none');
+  const [testingDisplay, setTestingDisplay] = React.useState('none');
+  const [developmentDisplay, setDevelopmentDisplay] = React.useState('none');
+
+  const handleFrontendDisplay = () => {
+    setFrontendDisplay((frontendDisplay === 'none') ? '' : 'none');
+  }
+
+  const handleBackendDisplay = () => {
+    setBackendDisplay((backendDisplay === 'none') ? '' : 'none');
+  }
+
+  const handleTestingDisplay = () => {
+    setTestingDisplay((testingDisplay === 'none') ? '' : 'none');
+  }
+
+  const handleDevelopmentDisplay = () => {
+    setDevelopmentDisplay((developmentDisplay === 'none') ? '' : 'none');
+  }
 
   return (
     <Grid
@@ -25,20 +46,28 @@ const SectionSkills = () => {
     >
       <h2>Skills</h2>
       <Grid item className={gridCategoryClass.root} >
-        <Button>Frontend</Button>
-        <FrontendItems />
+        <Button onClick={handleFrontendDisplay}>Frontend</Button>
+        <Box display={frontendDisplay}>
+          <FrontendItems />
+        </Box>
       </Grid>
       <Grid item className={gridCategoryClass.root}>
-        <Button>Backend</Button>
-        <BackendItems />
+        <Button onClick={handleBackendDisplay}>Backend</Button>
+        <Box display={backendDisplay}>
+          <BackendItems />
+        </Box>
       </Grid>
       <Grid item className={gridCategoryClass.root}>
-        <Button>Testing</Button>
-        <TestingItems />
+        <Button onClick={handleTestingDisplay}>Testing</Button>
+        <Box display={testingDisplay}>
+          <TestingItems />
+        </Box>
       </Grid>
       <Grid item className={gridCategoryClass.root}>
-        <Button>Development</Button>
-        <DevelopmentItems />
+        <Button onClick={handleDevelopmentDisplay}>Development</Button>
+        <Box display={developmentDisplay}>
+          <DevelopmentItems />
+        </Box>
       </Grid>
     </Grid>
   )
