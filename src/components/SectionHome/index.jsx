2812e7bee66592e5e160b59fedd0f9c881e96e25
Tandy Nguyen <nguyetan001@gmail.com>
2021-03-04 10:44:00 -0500

    Move section to separate folder

--- src/components/SectionHome/index.jsx
+++ src/components/SectionHome/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
-import ImageSelf from '../../images/me.png';
+import ImageSelf from '../../../images/me.png';
 
 const gridContainer = makeStyles({
   root: {
