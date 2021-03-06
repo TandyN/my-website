7b34d8a9c4bcf82258be3c2627153d006ac58c14
Tandy Nguyen <nguyetan001@gmail.com>
2021-03-06 12:53:45 -0500

    Adjust home page size

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuBar from './MenuBar';
 import SectionHome from './SectionHome';
@@ -11,7 +12,9 @@ import SectionPersonal from './SectionPersonal';
 
 const appBox = makeStyles({
   root: {
-    backgroundColor: '#333',
+    fontFamily: 'sans-serif',
+    height: '100%',
+    marginTop: '64px',
   }
 });
 
@@ -25,6 +28,7 @@ const App = () => {
   return (
     <Container
       maxWidth="xl"
+      className={appBoxClass.root}
     >
       <MenuBar hanldeMenuBarClick={hanldeMenuBarClick} />
       <SectionHome />
