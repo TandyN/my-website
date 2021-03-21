a52e9edaabed40b81e5b11fefdb907d0c2d865bd
Tandy Nguyen <nguyetan001@gmail.com>
2021-03-21 06:56:20 -0400

    Add Experience List Small component

---
+++ src/components/SectionExperience/ExperienceListSmall.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import Hidden from '@material-ui/core/Hidden';
+import { makeStyles } from '@material-ui/core/styles';
+
+const styledCompanyAndTitle = makeStyles({
+  root: {
+    textAlign: 'center',
+    margin: 10,
+  }
+});
+
+const ExperienceListSmall = ({ experienceData }) => {
+  const styledCompanyAndTitleClass = styledCompanyAndTitle();
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpand = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <Hidden mdUp>
+      <Grid
+        container
+        direction="column"
+        justify="space-evenly"
+      >
+        {experienceData.map((experienceItem, index) => {
+          return (
+            <Grid
+              item
+              key={index}
+            >
+              <Accordion
+                expanded={expanded === `panel${index}`}
+                onChange={handleExpand(`panel${index}`)}
+              >
+                <AccordionSummary>
+                  <Grid
+                    container
+                    direction="column"
+                    alignItems="center"
+                    spacing={0}
+                  >
+                    <h3 className={styledCompanyAndTitleClass.root}>{`${experienceItem.start_year} - ${experienceItem.end_year}`}</h3>
+                    <h3 className={styledCompanyAndTitleClass.root}>{experienceItem.company}</h3>
+                    <h4 className={styledCompanyAndTitleClass.root}>{experienceItem.position_title}</h4>
+                  </Grid>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <h5>{experienceItem.description}</h5>
+                </AccordionDetails>
+              </Accordion>
+            </Grid>
+          )
+        })}
+      </Grid>
+    </Hidden>
+  )
+}
+
+export default ExperienceListSmall;
--- src/components/SectionExperience/index.jsx
+++ src/components/SectionExperience/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import ExperienceHeader from './ExperienceHeader';
+import ExperienceListSmall from './ExperienceListSmall';
 import ExperienceListLarge from './ExperienceListLarge';
 
 import experienceData from './experience.json';
@@ -25,6 +26,7 @@ const SectionExperience = () => {
     >
       <ExperienceHeader />
       <ExperienceListLarge experienceData={experienceData} />
+      <ExperienceListSmall experienceData={experienceData} />
     </Grid>
   )
 }
