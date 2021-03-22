0abff31526e2f0ed74fc797112349aeb868eff94
Tandy Nguyen <nguyetan001@gmail.com>
2021-03-22 07:55:01 -0400

    Edit personal section

--- src/components/SectionPersonal/index.jsx
+++ src/components/SectionPersonal/index.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const styledPersonalSectionWrapper = makeStyles({
+  root: {
+    height: '50%',
+  }
+});
 
 const SectionPersonal = () => {
+  const styledPersonalSectionWrapperClass = styledPersonalSectionWrapper();
+
   return (
-    <div>Personal</div>
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justify="space-evenly"
+      className={styledPersonalSectionWrapperClass.root}
+    >
+      <h2>Personal</h2>
+      <p>Description</p>
+    </Grid>
   )
 }
 
